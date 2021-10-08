aee9d119e3b19997c5e7f4bbe26f927360758540
Zeilstra,Tjerk T <t.zeilstra@student.fontys.nl>
2021-10-08 23:56:44 +0200

    Begin DAL

---
+++ DAL/Helper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+
+namespace DAL
+{
+    public static class Helper
+    {
+        public static string ConString(IConfigurationBuilder configurationBuilder)
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+            configurationBuilder.AddJsonFile(path);
+            return string.Empty;
+        }
+    }
+}
